05599f24195cb82da6d8d5790c45fbf26c1e1d18
Dídac <didactalens@gmail.com>
2021-05-16 21:06:20 +0200

    Constructor creado

--- src/Objeto.java
+++ src/Objeto.java
@@ -2,4 +2,10 @@ public class Objeto {
     protected String nombre;
     protected double precio;
     protected boolean caducado;
+
+    public Objeto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.caducado = false;
+    }
 }
